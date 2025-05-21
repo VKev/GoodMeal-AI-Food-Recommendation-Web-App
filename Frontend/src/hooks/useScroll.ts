22472bea86226aa27e780eee480f607c86a2f553
Gwang <114713193+gwang08@users.noreply.github.com>
2025-05-21 09:02:56 +0700

    Delete Frontend directory

--- Frontend/src/hooks/useScroll.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function useScroll() {
-  return (
-  React
-  )
-}
-
-export default useScroll
--- Frontend/src/services/Food.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Food() {
-  return (
-  React
-  )
-}
-
-export default Food
