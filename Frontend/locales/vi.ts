2371bcb62ae2e3c5133cb852f8e08975c59b67a3
Gwang <114713193+gwang08@users.noreply.github.com>
2025-05-28 13:11:39 +0700

    feat: Make UI for Homepage

---
+++ Frontend/locales/vi.ts
@@ -0,0 +1,10 @@
+const vi = {
+    helpTitle: "Tôi có thể giúp gì?",
+    helpDescription: "Chúng tôi cung cấp các tiện ích như:",
+    features: [
+        "Gợi ý khi bạn gặp khó khăn trong việc tìm kiếm",
+        "Giúp bạn lựa chọn cửa hàng hoặc món ăn phù hợp",
+        "Giới thiệu về các tiện ích nổi bật"
+    ]
+};
+export default vi;
