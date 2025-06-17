164ad49a91648e126fd0dd62b16626ab09b739da
CuongNC-08 <cuong.nc.0804@gmail.com>
2025-06-17 21:24:08 +0700

    update

---
+++ Backend/Microservices/Prompt.Microservice/src/Domain/Entities/MessageRestaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure;
+
+public partial class MessageRestaurant
+{
+    public Guid MessageId { get; set; }
+
+    public Guid RestaurantId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public string? DeletedBy { get; set; }
+
+    public virtual Message Message { get; set; } = null!;
+}
---
+++ Backend/Microservices/Prompt.Microservice/src/Domain/Entities/PromptSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure;
+
+public partial class PromptSession
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public string? DeletedBy { get; set; }
+
+    public string? SessionName { get; set; }
+
+    public virtual ICollection<Message> Messages { get; set; } = new List<Message>();
+}
